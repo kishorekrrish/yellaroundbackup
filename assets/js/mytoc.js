a7b1a1bf569c54a3bc75080deeec87e496ce49df
kishoreBandanadam <kishoreBandanadam@users.noreply.github.com>
2019-11-01 22:44:53 +0530

    go

--- assets/js/mytoc.js
+++ assets/js/mytoc.js
@@ -1,4 +1,4 @@
-var node = document.createElement("H2"); 
+var node = document.createElement("H3"); 
 node.setAttribute("id", "h2node");                // Create a <h2> node
 var textnode = document.createTextNode("Contents");      // Create a text node
 node.appendChild(textnode);                              // Append the text to <h2>
